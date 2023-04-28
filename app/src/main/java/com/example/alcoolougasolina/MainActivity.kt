40a09d5ea4fa5d7982fccbdbae4aab1266599fde
Paulo Nemer <93223341+paulonemer@users.noreply.github.com>
2023-04-28 13:09:58 -0300

    prymary changes

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
@@ -13,12 +16,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("PDM23","No onCreate, $percentual")
 
+
+
+
+        fun compara(){
+
+            val mySwitch = findViewById<Switch>(R.id.swPercentual)
+
+            mySwitch.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    percentual=0.75
+                } else {
+                    percentual=0.70
+                }
+            }
+
+            val valoralcoolet = findViewById<EditText>(R.id.edAlcool)
+            val valoralcool = valoralcoolet.text.toString().toDoubleOrNull()
+
+            val valorgasolinaet = findViewById<EditText>(R.id.edGasolina)
+            val valorgasolina = valorgasolinaet.text.toString().toDoubleOrNull()
+
+            if (valoralcool != null && valorgasolina != null) {
+                if (valoralcool <= valorgasolina * percentual) {
+                    Toast.makeText(this, "Álcool vale a pena", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Gasolina vale a pena", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
         val btCalc: Button = findViewById(R.id.btCalcular)
         btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
+            compara()
             Log.d("PDM23","No btCalcular, $percentual")
         })
+
+
+
+
     }
 override fun onResume(){
     super.onResume()
@@ -26,18 +61,18 @@ override fun onResume(){
 }
 override fun onStart(){
     super.onStart()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onStart")
 }
 override fun onPause(){
     super.onPause()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onPause")
 }
 override fun onStop(){
     super.onStop()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onStop")
 }
 override fun onDestroy(){
     super.onDestroy()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","onDestroy")
 }
 }
